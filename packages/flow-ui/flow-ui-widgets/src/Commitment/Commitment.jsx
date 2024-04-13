06b5c6bc2ab2cd9c519642766ff98065c85a9840
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2024-04-13 15:07:50 +0400

    More images

--- packages/flow-ui/flow-ui-widgets/src/Commitment/Commitment.jsx
+++ packages/flow-ui/flow-ui-widgets/src/Commitment/Commitment.jsx
@@ -11,9 +11,9 @@ const Commitment = props => {
   if (language === "es") {
     title = "¡Escríbeme!";
   }
-  let text = "Hello there! We're thrilled to be connected with you and help bring your ideas to life.";
+  let text = "Hello there! We're thrilled to be connected with you and help bring your ideas to life using A.I.";
   if (language === "es") {
-    text = "¡Hola! Estaré encantado de estar en contacto contigo y ayudarte a dar vida a tus ideas.";
+    text = "¡Hola! Estaré encantado de estar en contacto contigo y ayudarte a dar vida a tus ideas usando I.A.";
   }
   return (
     <Section aside title={title} {...props}>
